4b4ba67a901725e0651d1d77c1361497a3eba861
ovkachanov <ovkachanov1988@gmail.com>
2018-04-23 16:33:40 +0300

    bootstrap initialed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+
+  has_many :events
+
+  validates :name, presence: true, length: { maximum: 35 }
+  validates :email, presence: true, length: { maximum: 255}
+  validates :email, uniqueness: true
+  validates :email, format: { with: /@/ }
 end
