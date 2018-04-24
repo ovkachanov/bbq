c9f5c2819f27f12d6890558fce86f360de9d4665
ovkachanov <ovkachanov1988@gmail.com>
2018-04-24 16:39:23 +0300

    alpha version

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'jquery-rails'
 gem 'twitter-bootstrap-rails'
+gem 'devise'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   has_many :events
 
   validates :name, presence: true, length: { maximum: 35 }
-  validates :email, presence: true, length: { maximum: 255}
-  validates :email, uniqueness: true
-  validates :email, format: { with: /@/ }
+
+  before_validation :set_name, on: :create
+
+  private
+
+  def set_name
+    self.name = "User #{rand(800)}" if self.name.blank?
+  end
 end
