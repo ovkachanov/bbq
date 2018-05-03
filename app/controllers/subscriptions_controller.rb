630018b10e00d26700b58fe2eb7941535dcf2ca7
ovkachanov <ovkachanov1988@gmail.com>
2018-05-03 11:39:38 +0300

    the author can not subscribe to his event

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -7,7 +7,9 @@ class SubscriptionsController < ApplicationController
     @new_subscription = @event.subscriptions.build(subscription_params)
     @new_subscription.user = current_user
 
-    if @new_subscription.save
+    if @event.user == current_user
+      redirect_to @event, alert: I18n.t('controllers.subscriptions.author?')
+    elsif @new_subscription.save
       redirect_to @event, notice: I18n.t('controllers.subscriptions.created')
     else
       render 'events/show', alert: I18n.t('controllers.subscriptions.error')
