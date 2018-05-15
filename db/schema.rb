0319ef62f488dfce9b5553bf262a2883ba9d4c29
ovkachanov <ovkachanov1988@gmail.com>
2018-05-15 14:53:17 +0300

    an entry was added to the event of the secret pincode

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180505134713) do
+ActiveRecord::Schema.define(version: 20180515105010) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180505134713) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "pincode"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
